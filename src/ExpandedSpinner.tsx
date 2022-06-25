e2fbe30e663efd7d78dbe7878c35d5b8eb274769
ikotsov <i.kotsovilis@gmail.com>
2022-06-26 01:56:28 +0300

    refactor: rename component

--- src/ExpandedSpinner.tsx
+++ src/ExpandedSpinner.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import {ActivityIndicator, StyleSheet, View} from 'react-native';
 
-export function FullScreenSpinner() {
+export function ExpandedSpinner() {
   return (
     <View style={styles.fullscreen}>
       <ActivityIndicator size="large" />
@@ -12,5 +12,6 @@ export function FullScreenSpinner() {
 const styles = StyleSheet.create({
   fullscreen: {
     flex: 1,
+    justifyContent: 'center',
   },
 });
