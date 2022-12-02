9588b15d9064801435733d419a71cb6eb3c1548a
ikotsov <i.kotsovilis@gmail.com>
2022-12-03 01:19:14 +0200

    add: data validation after fetching

--- src/service/planets-fetcher.ts
+++ src/service/planets-fetcher.ts
@@ -1,6 +1,12 @@
+import {isPlanetListResponse} from './data-validation';
+
 const MAIN_URL = 'https://swapi.dev/api/planets';
 
 export const fetchPlanets = async (page: string) => {
   const response = await fetch(`${MAIN_URL}?page=${page}`);
-  return response.json();
+  const data = await response.json();
+  if (isPlanetListResponse(data)) {
+    return data;
+  }
+  throw new Error('Planet data not valid');
 };
