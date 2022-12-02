4ca447de1bc5df1fb99dc5cab8e3b75428f00d2c
ikotsov <i.kotsovilis@gmail.com>
2022-12-03 01:29:26 +0200

    add: content component
    Set a mock fetch with a timeout.

--- App.tsx
+++ App.tsx
@@ -1,18 +1,38 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {SafeAreaView, StyleSheet, Text} from 'react-native';
 import {ExpandedSpinner} from './src/components/expanded-spinner';
+import {usePlanets} from './src/service/use-planets';
 
 function App() {
   return (
     <SafeAreaView style={styles.container}>
       <Headline>Planethopper</Headline>
-      <ExpandedSpinner />
+      <Content />
     </SafeAreaView>
   );
 }
 
 function Headline({children}: {children: string}) {
   return <Text style={styles.headline}>{children}</Text>;
+}
+
+function Content() {
+  const {planets, currentPage, isLoading, fetchMorePlanets} = usePlanets();
+  console.log({planets, currentPage, isLoading});
+
+  useEffect(() => {
+    const id = setTimeout(() => {
+      fetchMorePlanets();
+    }, 5000);
+
+    return () => clearTimeout(id);
+  }, [fetchMorePlanets]);
+
+  if (isLoading) {
+    return <ExpandedSpinner />;
+  }
+
+  return null;
 }
 
 const styles = StyleSheet.create({
