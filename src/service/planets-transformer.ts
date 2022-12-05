3da51542f57531685c6a162f7c5f9e5c80e3af96
ikotsov <i.kotsovilis@gmail.com>
2022-12-05 20:09:57 +0200

    add: population format

--- src/service/planets-transformer.ts
+++ src/service/planets-transformer.ts
@@ -1,6 +1,7 @@
 import {idCreator} from '../idCreator';
 import {PlanetType} from '../planet-type';
 import {pickImageUrlRandomly} from '../random-image-url-picker';
+import {formatInUSLocale} from '../us-locale-formatter';
 import {PlanetResponse} from './data-validation';
 
 export const transformPlanets = (planets: PlanetResponse[]): PlanetType[] => {
@@ -8,12 +9,14 @@ export const transformPlanets = (planets: PlanetResponse[]): PlanetType[] => {
 };
 
 const transformPlanet = (planet: PlanetResponse): PlanetType => {
+  const population = Number(planet.population);
+
   return {
     terrain: planet.terrain,
     climate: planet.climate,
     image: pickImageUrlRandomly(),
     name: planet.name,
-    population: planet.population,
+    population: isNaN(population) ? 'unknown' : formatInUSLocale(population),
     id: createPlanetId(),
   };
 };
