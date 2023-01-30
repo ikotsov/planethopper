78dc1b3446f2f7d125f0da39dbcc772b0111cbeb
ikotsov <i.kotsovilis@gmail.com>
2023-01-30 11:53:57 +0200

    add: render trip planner

--- App.tsx
+++ App.tsx
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {FlatList, SafeAreaView, StyleSheet, Text} from 'react-native';
 import {ExpandedSpinner} from './src/components/expanded-spinner';
 import {Planet} from './src/components/planet';
+import {TripPlanner} from './src/components/trip-planner';
 import {VerticalSpacer} from './src/components/vertical-spacer';
 import {PlanetType} from './src/planet-type';
 import {usePlanets} from './src/service/use-planets';
@@ -22,19 +23,27 @@ function Headline({children}: {children: string}) {
 
 function Content() {
   const {planets, isLoading, fetchMore} = usePlanets();
+  const [selectedPlanets, setSelectedPlanets] = useState<string[]>([]);
 
   if (isLoading) {
     return <ExpandedSpinner />;
   }
 
   return (
-    <FlatList
-      data={planets}
-      renderItem={renderItem}
-      keyExtractor={item => item.id}
-      ItemSeparatorComponent={() => <VerticalSpacer size={10} />}
-      onEndReached={fetchMore}
-    />
+    <>
+      <FlatList
+        data={planets}
+        renderItem={renderItem}
+        keyExtractor={item => item.id}
+        ItemSeparatorComponent={() => <VerticalSpacer size={10} />}
+        onEndReached={fetchMore}
+      />
+      <TripPlanner
+        isVisible={selectedPlanets.length > 0}
+        destinations={selectedPlanets}
+        onClear={() => setSelectedPlanets([])}
+      />
+    </>
   );
 }
 
